2e9053a8011d278096afc985f6ec4914aa886da2
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-07-25 23:40:48 +0200

    chore: prepare services for user

---
+++ src/services/expanses/expanse.service.ts
@@ -0,0 +1,1 @@
+﻿// Backend logic for the expanses
---
+++ src/services/user/user.service.ts
@@ -0,0 +1,19 @@
+﻿import { prisma } from "@/lib/prismaClient";
+
+export const createUser = async (id: string) => {
+	const user = await prisma.user.upsert({
+		where: { clerkUserId: id },
+		update: { clerkUserId: id },
+		create: { clerkUserId: id },
+	});
+
+	return user;
+};
+
+export const deleteUser = async (id: string) => {
+	const user = await prisma.user.delete({
+		where: { clerkUserId: id },
+	});
+
+	return user;
+};
