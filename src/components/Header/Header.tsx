ad00c023bccc04ca81e1f48e3092dd8f05c6346b
kacper-echaust <k9echaust@gmail.com>
2024-08-01 19:56:48 +0200

    display navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
-﻿import { ReceiptViewer } from "../ReceiptViewer/ReceiptViewer";
-import styles from "./Header.module.css";
+﻿"use client";
+import { useState } from "react";
+import { Logo } from "./Logo/Logo";
+import styles from "./Header.module.scss";
+import { BurgerMenu } from "./BurgerMenu/BurgerMenu";
+import { MenuList } from "./MenuList/MenuList";
 
 export const Header = () => {
+	const [showMenu, setShowMenu] = useState(false);
+	const handleShowMenu = () => {
+		setShowMenu(!showMenu);
+	};
 	return (
-		<header className={styles.header}>
-			This is header
-			<ReceiptViewer />
+		<header>
+			<nav className={styles.nav}>
+				<Logo />
+				<BurgerMenu onClick={handleShowMenu} />
+			</nav>
+			{showMenu && <MenuList />}
 		</header>
 	);
 };
