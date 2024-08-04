4195fc5185e7447cba0b8575e89b55c04857bc39
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2024-08-04 20:51:19 +0200

    feat: create ExtractingError component

---
+++ src/components/ExtractingError/ExtractingError.tsx
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import Link from "next/link";
+import styles from "./ExtractingError.module.scss";
+import { Logo } from "@/components/Logo/Logo";
+import { UploadPhotoInput } from "@/components/UploadPhoto/UploadPhotoInput";
+
+export const ExtractingError = () => {
+	return (
+		<div className={styles.wrapper}>
+			<Logo width={39} height={29} />
+			<p className={styles.info}>
+				<span>Jeszcze raz.</span>
+				<br />
+				Nie znaleźliśmy danych z paragonu
+			</p>
+			<div className={styles.btnsWrapper}>
+				<UploadPhotoInput label="Spróbuj ponownie" className={styles.uploadPhotoInput} />
+
+				<Link href={"/dashboard"} className={styles.btnOutline}>
+					Przejdź do strony głównej
+				</Link>
+			</div>
+		</div>
+	);
+};
