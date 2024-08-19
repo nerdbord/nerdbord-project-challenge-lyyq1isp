4eef3164e9f121d1c43604d4f7fda1760e7636fb
Mateusz <matmatwiej@gmail.com>
2024-08-19 10:27:56 +0200

    Chore/add reusable components (#16)
    * chore: create css variables
    
    * chore: create reusable components; add icons

---
+++ src/assets/icons/ArrowLeft.tsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+
+export const ArrowLeft = ({ color = "#00A3C4" }: { color?: string }) => {
+	return (
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M21 12H3M3 12L10 5M3 12L10 19"
+				stroke={color}
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
---
+++ src/assets/icons/IdeaIcon.tsx
@@ -0,0 +1,30 @@
+﻿export const IdeaIcon = () => {
+	return (
+		<svg
+			width="25"
+			height="25"
+			viewBox="0 0 25 25"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+		>
+			<rect width="25" height="25" fill="url(#pattern0_1346_6277)" />
+			<defs>
+				<pattern
+					id="pattern0_1346_6277"
+					patternContentUnits="objectBoundingBox"
+					width="1"
+					height="1"
+				>
+					<use xlinkHref="#image0_1346_6277" transform="scale(0.0208333)" />
+				</pattern>
+				<image
+					id="image0_1346_6277"
+					width="48"
+					height="48"
+					xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAACc0lEQVR4nO2ZzWoUQRDHS59D/EBU8CR6EQKiPoIfRNCH8GZumiD4GLpVsxcR30CTbNUQX0ANuUkMqCgakwhhxZHuZeJudmad7q3qZGH/0LAMvT3/X1d1z0w1gJVIioE2caIpwAFrGgEttZfOHFgE2pH3/meC7wPKbyC5B6mFfBtQukA8F2++nL3UEFiaLyMYCtHqnB0cwEN0IZNZsFYms5X3dp50BuIbVt7Bja06cfshkDdGzgQtXwTkeUBZBOT3QLLtm/vduzbv+4yMPG/oRr2EqDNfFEeA8luAvDq089Q11xflpv9vHYRqyrrQVpnH5VOAstLY+FDjHJ6unKyEsExVr6wzA8Sf483vQXyFTK7Zmh0yn18B5N3xzUuZUrt+zCRyISf5omae+iLxTE7bmneLbqycb7AmioqFrfqENDMvvWa2eP3sB2yV8VF4ZwPQ5kv25qXXMrmgD4CykAyA+JEFwGJCgFfNjdUNMgywlgwAebW5z+YAW+kAZMsAgH+lSyHZtgBYTwjwITrVa0WylC6F+DWoC+VJQoDHRq/PqVKIL0/uqwTKWzCTK62YA+R37AAeFkcn+3XaqcXHAfmbAcB3/42dRJhfB+IdxZnfAeKracz3Q6hEghN81NeVVdz3MYrEL1juQJafsC2rNClsuZ0j1Lwbt7AubO2vEiN/Auqcr+wbClB3HkD8UacqHlrc1QBoaRV3Y8rrGgCq5XV/OsN/9kKJcnd0fyUAJ18sHkjdBzDGEVP3v+a1Afohos2HHrQR/wzYPn+o3ltFKC8DHl4v4NCJ5FyjJ7Prk3RmQ0RvjgHJc0DerDC+6Wf+0JqfCqL0F93ptynWn9aoAAAAAElFTkSuQmCC"
+				/>
+			</defs>
+		</svg>
+	);
+};
